59a486cae8aacf3158ce0023a16fa23e662d9488
Dmitriy Usic <usyasha69@mail.ru>
2016-09-07 21:19:19 +0300

    first commit

---
+++ src/com/company/collections/two_lists/Main.java
@@ -0,0 +1,10 @@
+package com.company.collections.two_lists;
+
+public class Main {
+    public static void main(String[] args) {
+        ListsWorker listsWorker = new ListsWorker();
+
+        listsWorker.foundDifferentElement(110, 109);
+        listsWorker.foundDifferentElement(148, 147);
+    }
+}
