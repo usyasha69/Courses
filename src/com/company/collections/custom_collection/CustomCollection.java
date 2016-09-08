94691e95d2eed7d01df8c774e55f0335efdc80b6
Dmitriy Usic <usyasha69@mail.ru>
2016-09-08 22:45:11 +0300

    first commit

--- src/com/company/collections/custom_collection/CustomCollection.java
+++ src/com/company/collections/custom_collection/CustomCollection.java
@@ -1,25 +1,43 @@
 package com.company.collections.custom_collection;
 
 public class CustomCollection<E> implements com.company.collections.custom_collection.List<E> {
-
+    /**
+     * Data of custom collection.
+     */
     private Object[] data;
+    /**
+     * Size of custom collection
+     */
     private int size;
 
+    /**
+     * Default constructor create new data in custom collection if initial capacity not defined.
+     */
     public CustomCollection() {
         this.data = new Object[16];
     }
 
+    /**
+     * Constructor create new data in custom collection given by initial capacity.
+     *
+     * @param initialCapacity - start capacity of custom collection
+     */
     public CustomCollection(int initialCapacity) {
-
+        //if initial capacity less zero throw new excetion
         if (initialCapacity < 0) {
             throw new IllegalArgumentException("Illegal capacity: " + initialCapacity);
         }
 
         this.data = new Object[initialCapacity];
-
         this.size = initialCapacity;
     }
 
+    /**
+     * This method add new element to custom collection.
+     *
+     * @param element - element want to add
+     * @return - added element or not
+     */
     @Override
     public boolean add(E element) {
         int freeElementIndex = 0;
@@ -50,17 +68,59 @@ public class CustomCollection<E> implements com.company.collections.custom_colle
         return result;
     }
 
+    /**
+     * This method add element to custom collection for a given index.
+     *
+     * @param index   - index of position where it is necessary to add
+     * @param element - element want to add
+     */
     @Override
     public void add(int index, E element) {
-        Object[] newData = new Object[data.length + 1];
+        //size of collection
+        int dataLength = 0;
+
+        //calculate size of collection
+        for (Object aData : data) {
+            if (aData != null) {
+                dataLength++;
+            }
+        }
+
+        //if size of custom collection equal zero
+        if (dataLength == 0) {
+            Object[] newData = new Object[dataLength + 1];
+            newData[0] = element;
+
+            data = newData;
+            size = data.length;
+
+            return;
+        }
+
+        //if length of index more size of custom collection
+        if (index > dataLength - 1) {
+            Object[] newData = new Object[dataLength + 1];
+            for (int i = 0; i < dataLength; i++) {
+                newData[i] = data[i];
+            }
+
+            newData[newData.length - 1] = element;
+
+            data = newData;
+            size = data.length;
+
+            return;
+        }
+
+        Object[] newData = new Object[dataLength + 1];
 
         for (int i = 0; i < index; i++) {
-            newData[i] = data;
+            newData[i] = data[i];
         }
 
         newData[index] = element;
 
-        for (int i = index + 1; i <= data.length; i++) {
+        for (int i = index + 1; i < newData.length; i++) {
             newData[i] = data[i - 1];
         }
 
@@ -68,8 +128,15 @@ public class CustomCollection<E> implements com.company.collections.custom_colle
         size = data.length;
     }
 
+    /**
+     * This method return to element for a given index.
+     *
+     * @param index - index of element
+     * @return - element
+     */
     @Override
     public E get(int index) {
+
         if (index >= 0 && index < size) {
             return (E) data[index];
         } else {
@@ -77,6 +144,12 @@ public class CustomCollection<E> implements com.company.collections.custom_colle
         }
     }
 
+    /**
+     * This method removed element for a given index.
+     *
+     * @param index - index of removed element
+     * @return - removed element
+     */
     @Override
     public E remove(int index) {
         //result
@@ -91,8 +164,8 @@ public class CustomCollection<E> implements com.company.collections.custom_colle
         }
 
         //calculate size of collection
-        for (int i = 0; i < data.length; i++) {
-            if (data[i] != null) {
+        for (Object aData : data) {
+            if (aData != null) {
                 dataLength++;
             }
         }
@@ -123,7 +196,7 @@ public class CustomCollection<E> implements com.company.collections.custom_colle
             Object[] newData = new Object[dataLength - 1];
 
             System.arraycopy(data, 0, newData, 0, index);
-            System.arraycopy(data, index, newData, index - 1, dataLength - index);
+            System.arraycopy(data, index + 1, newData, index, dataLength - 1 - index);
 
             data = newData;
             size = newData.length;
@@ -132,16 +205,29 @@ public class CustomCollection<E> implements com.company.collections.custom_colle
         return element;
     }
 
+    /**
+     * This method check is custom collection are empty or not.
+     *
+     * @return - collection empty or not
+     */
     @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * This method return size of custom collection.
+     *
+     * @return - size of custom collection
+     */
     @Override
     public int size() {
         return size;
     }
 
+    /**
+     * This method clear all elements in custom collection.
+     */
     @Override
     public void clear() {
         for (int i = 0; i < data.length; i++) {
--- src/com/company/collections/custom_collection/Tester.java
+++ src/com/company/collections/custom_collection/Tester.java
@@ -2,32 +2,92 @@ package com.company.collections.custom_collection;
 
 public class Tester {
 
+    /**
+     * This method tested custom collection.
+     */
     public void testCustomCollection() {
-        CustomCollection<Integer> customCollection = new CustomCollection<>();
+        System.out.println("Test of first default constructor: ");
+        CustomCollection<Integer> firstCustomCollection = new CustomCollection<>();
 
-        customCollection.add(0);
-        customCollection.add(1);
-        customCollection.add(2);
-        customCollection.add(3);
-        customCollection.add(4);
+        System.out.println("Size of collection: " + firstCustomCollection.size());
 
-        customCollection.remove(0);
-        System.out.println(customCollection.size());
+        System.out.println("\nTest of second constructor");
+        CustomCollection<Integer> secondCustomCollection = new CustomCollection<>(5);
 
-//        System.out.println(customCollection.size());
-//
-//        customCollection.remove(0);
-//
-//        customCollection.clear();
-//
-//        if (customCollection.isEmpty()) {
-//            System.out.println("Empty");
-//        }
-//
-        for (int i = 0; i < customCollection.size(); i++) {
-            System.out.println(customCollection.get(i));
+        System.out.println("Size of collection: " + secondCustomCollection.size());
+
+        System.out.println("\nTest of method add(E element) and method get(index i) in first collection: ");
+        for (int i = 0; i < 5; i++) {
+            firstCustomCollection.add(i);
         }
 
+        System.out.println("Size of first collection: " + firstCustomCollection.size() + "\n");
+        for (int i = 0; i < firstCustomCollection.size(); i++) {
+            System.out.println(i + " element in first collection: " + firstCustomCollection.get(i));
+        }
 
+        System.out.println("\nTest of method add(E element) and method get(index i) in second collection: ");
+        for (int i = 0; i < 5; i++) {
+            secondCustomCollection.add(i);
+        }
+
+        System.out.println("Size of second collection: " + secondCustomCollection.size() + "\n");
+        for (int i = 0; i < secondCustomCollection.size(); i++) {
+            System.out.println(i + " element in second collection: " + secondCustomCollection.get(i));
+        }
+
+        System.out.println("\nTest of method add(int index, E element) in first collection: ");
+        firstCustomCollection.add(0, 322);
+        firstCustomCollection.add(3, 322);
+        firstCustomCollection.add(5, 322);
+
+        System.out.println("Size of first collection: " + firstCustomCollection.size() + "\n");
+        for (int i = 0; i < firstCustomCollection.size(); i++) {
+            System.out.println(i + " element in first collection: " + firstCustomCollection.get(i));
+        }
+
+        System.out.println("\nTest of method add(int index, E element) in second collection: ");
+        secondCustomCollection.add(0, 322);
+        secondCustomCollection.add(3, 322);
+        secondCustomCollection.add(5, 322);
+
+        System.out.println("Size of second collection: " + secondCustomCollection.size() + "\n");
+        for (int i = 0; i < secondCustomCollection.size(); i++) {
+            System.out.println(i + " element in second collection: " + secondCustomCollection.get(i));
+        }
+
+        System.out.println("\nTest of method remove(int index) in first collection: ");
+        firstCustomCollection.remove(0);
+        firstCustomCollection.remove(2);
+        firstCustomCollection.remove(3);
+
+        System.out.println("Size of first collection: " + firstCustomCollection.size() + "\n");
+        for (int i = 0; i < firstCustomCollection.size(); i++) {
+            System.out.println(i + " element in first collection: " + firstCustomCollection.get(i));
+        }
+
+        System.out.println("\nTest of method remove(int index) in second collection: ");
+        secondCustomCollection.remove(0);
+        secondCustomCollection.remove(2);
+        secondCustomCollection.remove(3);
+
+        System.out.println("Size of second collection: " + secondCustomCollection.size() + "\n");
+        for (int i = 0; i < secondCustomCollection.size(); i++) {
+            System.out.println(i + " element in second collection: " + secondCustomCollection.get(i));
+        }
+
+        System.out.println("\nTest of method clear() and isEmpty() in first collection: ");
+        firstCustomCollection.clear();
+        System.out.println("Size of first collection: " + firstCustomCollection.size() + "\n");
+        if (firstCustomCollection.isEmpty()) {
+            System.out.println("First collection is empty");
+        }
+
+        System.out.println("\nTest of method clear() and isEmpty() in second collection: ");
+        secondCustomCollection.clear();
+        System.out.println("Size of second collection: " + secondCustomCollection.size() + "\n");
+        if (secondCustomCollection.isEmpty()) {
+            System.out.println("Second collection is empty");
+        }
     }
 }
