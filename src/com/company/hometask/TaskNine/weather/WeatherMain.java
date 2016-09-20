3280e6f69313e990a421206503a8812d573cbf82
Dmitriy Usic <usyasha69@mail.ru>
2016-09-20 19:30:15 +0300

    first commit

--- src/com/company/hometask/TaskNine/weather/WeatherMain.java
+++ src/com/company/hometask/TaskNine/weather/WeatherMain.java
@@ -2,26 +2,23 @@ package com.company.hometask.TaskNine.weather;
 
 public class WeatherMain {
 
+    public static Weather weather;
+
     public static void main(String[] args) {
-        final int WEATHER_NUMBER = 5;
+        final int NUMBER_UPDATES = 14;
 
-        for (int i = 0; i < WEATHER_NUMBER; i++) {
-            Weather weather = WeatherGenerator.generateWeather();
+        new WeatherThread("English").start();
+        new WeatherThread("Russian").start();
+        new WeatherThread("Germany").start();
 
-            new WeatherThread(WeatherVocabulary.translateWeather(weather, "English"), "English").start();
-
-            new WeatherThread(WeatherVocabulary.translateWeather(weather, "Russian"), "Russian").start();
-
-            new WeatherThread(WeatherVocabulary.translateWeather(weather, "Germany"), "Germany").start();
+        for (int i = 0; i < NUMBER_UPDATES; i++) {
+            weather = WeatherGenerator.generateWeather();
 
             try {
-                Thread.sleep(1500);
+                Thread.sleep(10000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
-            System.out.println();
         }
-
     }
 }
--- src/com/company/hometask/TaskNine/weather/WeatherThread.java
+++ src/com/company/hometask/TaskNine/weather/WeatherThread.java
@@ -2,11 +2,9 @@ package com.company.hometask.TaskNine.weather;
 
 public class WeatherThread extends Thread {
 
-    private Weather weather;
     private String language;
 
-    public WeatherThread(Weather weather, String language) {
-        this.weather = weather;
+    public WeatherThread(String language) {
         this.language = language;
     }
 
@@ -14,6 +12,39 @@ public class WeatherThread extends Thread {
     public void run() {
         super.run();
 
-        System.out.println(language + " weather thread: \n" + weather.toString());
+        final int NUMBER_UPDATES = 20;
+
+        for (int i = 0; i < NUMBER_UPDATES; i++) {
+            if (language.equals("English")) {
+
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(language + " weather thread: \n" + WeatherMain.weather.toString());
+            }
+
+            if (language.equals("Russian")) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(language + " weather thread: \n" + WeatherMain.weather.toString());
+            }
+
+            if (language.equals("Germany")) {
+                try {
+                    Thread.sleep(7000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(language + " weather thread: \n" + WeatherMain.weather.toString());
+            }
+        }
     }
 }
